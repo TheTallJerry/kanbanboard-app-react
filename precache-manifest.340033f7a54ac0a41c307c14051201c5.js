3c9c23b3a54d30019ba7e152b3432ffd021ea8aa
TheTallJerry <ziyuanzhang2019@gmail.com>
2023-02-25 20:53:41 -0500

    Updates

---
+++ precache-manifest.340033f7a54ac0a41c307c14051201c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec0bfc905d66a567b705",
+    "url": "/kanbanboard-app-react/static/css/main.7852b077.chunk.css"
+  },
+  {
+    "revision": "ec0bfc905d66a567b705",
+    "url": "/kanbanboard-app-react/static/js/main.ec0bfc90.chunk.js"
+  },
+  {
+    "revision": "dc2f8d4ee03398cb0a0f",
+    "url": "/kanbanboard-app-react/static/js/1.dc2f8d4e.chunk.js"
+  },
+  {
+    "revision": "3c01b482f39d32814648",
+    "url": "/kanbanboard-app-react/static/js/2.3c01b482.chunk.js"
+  },
+  {
+    "revision": "c607db09979dda57f538",
+    "url": "/kanbanboard-app-react/static/js/runtime~main.c607db09.js"
+  },
+  {
+    "revision": "d07a956f0dca5104d029b099f8d722e2",
+    "url": "/kanbanboard-app-react/index.html"
+  }
+];
